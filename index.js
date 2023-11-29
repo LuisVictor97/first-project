bef45f464f1c278c1234d07a84f3edf7800beed6
LuisVictor97 <luis.victor6904@gmail.com>
2023-11-29 17:01:18 -0300

    inicio projeto Felipão

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("olá mundão, testando commit")
+//definindo o nome e quantidade de XP do herói
+let nomeHero = "Charizard"
+let quantidadeXp = "1000"
+
+// utilizando uma estrutura de decisão para definir o nível do herói
+let nivelHero = ""
+
+if(quantidadeXp <= 1000){
+    nivelHero = "Ferro"
+}else if(quantidadeXp >= 1001 && quantidadeXp <= 2000 ){
+    nivelHero = "Bronze"
+}else if(quantidadeXp >=2001 && quantidadeXp <= 3000){    
+    nivelHero = "Prata"
+}else{
+    nivelHero = "Radiante"
+}
+
+console.log("O Herói de nome " + nomeHero + "está no nível " + nivelHero)  
